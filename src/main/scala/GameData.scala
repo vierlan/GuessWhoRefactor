2979cd56e0ce746b26e23fadec4a0b34b27fced1
Farnoosh Moayeri <farnooshmoayeri@gmail.com>
2025-02-27 16:25:17 +0000

    Adding hint to the game

--- src/main/scala/GameData.scala
+++ src/main/scala/GameData.scala
@@ -6,7 +6,7 @@ object GameData {
     Character("Ronaldo", "Male", "Black", hasGlasses = true, hasHat = false),
     Character("Robyn", "Female", "Red", hasGlasses = false, hasHat = true),
     Character("Mary", "Female", "Brown", hasGlasses = true, hasHat = false),
-    Character("Halaand", "Male", "Blonde", hasGlasses = false, hasHat = false),
+    Character("Haaland", "Male", "Blonde", hasGlasses = false, hasHat = false),
     Character("Dorothea", "Female", "Brown", hasGlasses = false, hasHat = true),
     Character("Sandra", "Female", "Black", hasGlasses = false, hasHat = false)
   )
